1a86e1f31d5e310e4fd8825be2b28eb181c889a3
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2024-08-27 17:10:49 +0200

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,6 @@ export default function Home() {
               whileInView={{ opacity: 1, scale: 1 }}
               whileHover={{ scale: 1.05 }}
               transition={{ duration: 0.3 }}
-              viewport={{ once: true }}
             >
               <Image
                 src={item.src}
@@ -187,7 +186,11 @@ export default function Home() {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1 }}
+        viewport={{ once: true }}
       >
+        {/* <div className={styles.reversedRoundedDivWrapper} data-scroll-progress={scrollProgress}>
+          <div className={styles.reversedRoundedDiv}></div>
+        </div> */}
         <Image
           src="https://utfs.io/f/a25e85e9-92b9-42fc-a7c2-c9dbd865284a-p1zbl.jpg"
           alt="Contact background"

c3f98c989ae70c5d216b4e1a679cf40d355d8369
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2024-09-03 17:32:31 +0200

    fix 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
       url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📸</text></svg>",
     },
   ],
+  themeColor: "#f8f8f8",
 }
 
 export default function RootLayout({
